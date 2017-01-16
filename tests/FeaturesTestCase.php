d039fbd32d7b3af352bff75763d96f6a11c8ca56
Luis Casiano <luis.casiano.ramos@gmail.com>
2017-01-16 00:57:49 -0500

    7 . Personalizar el directorio tests de tu aplicación

---
+++ tests/FeaturesTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Created by PhpStorm.
+ * User: piscouser
+ * Date: 16/01/2017
+ * Time: 12:52 AM
+ */
+class FeaturesTestCase extends TestCase
+{
+    use DatabaseTransactions;
+
+}
--- tests/features/ExampleTest.php
+++ tests/features/ExampleTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class ExampleTest extends FeaturesTestCase
 {
 
-    use DatabaseTransactions;
+
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    function test_basic_example()
     {
         $name='Luis Casiano';
         $email='luis.casiano.ramos@gmail.com';
