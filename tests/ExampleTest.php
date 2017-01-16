fd936057031769948c3830cd23526e3a733dd38d
Luis Casiano <luis.casiano.ramos@gmail.com>
2017-01-15 23:56:47 -0500

    configuracion-para-el-entorno-de-pruebas-automatizadas-phpunit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+
+    use DatabaseTransactions;
     /**
      * A basic functional test example.
      *
@@ -13,7 +15,15 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel Pymeayuda');
+        $name='Luis Casiano';
+        $email='luis.casiano.ramos@gmail.com';
+       $user=  factory(\App\User::class)->create([
+           'name'=>$name,
+           'email'=>$email,
+       ]);
+       $this->actingAs($user,'api')
+             ->visit('/api/user')
+             ->see($name)
+             ->see($email);
     }
 }
